0560f1769042ac678110ac0ee380f1d187829ed4
GodotMisogi <GodotMisogi@github.com>
2022-02-10 15:46:42 +0800

    Updated takeoff-empty XDSM

--- scripts/weight_estimation_xdsm.py
+++ scripts/weight_estimation_xdsm.py
@@ -16,16 +16,16 @@ x = XDSM.XDSM()
 # x.add_system('tail', ecomp, (r'\text{Tail}', r'\text{Sizing}'))
 
 #%% Mission
-x.add_system('mission', ecomp, (r'\text{Mission Segment}', r'\text{Fuel Fraction}'), stack=True,)# faded = True)
+x.add_system('mission', ecomp, (r'\text{Mission Segment}', r'\text{Fuel Fraction}'), stack=True, faded = True)
 
 #%% Fuel fractions
 x.add_system('ff', ecomp, (r'\text{Fuel-Weight}', r'\text{Estimation}'), faded = True)
 
 #%% Maximum takeoff weight
-x.add_system('mtow', icomp, (r'\text{Takeoff Weight }', r'\text{Estimation}'), faded = True)
+x.add_system('mtow', icomp, (r'\text{Takeoff Weight }', r'\text{Estimation}'),)# faded = True)
 
 #%% Empty weight
-x.add_system('empty', ecomp, (r'\text{Empty Weight}', r'\text{Estimation}'), faded = True)
+x.add_system('empty', ecomp, (r'\text{Empty Weight}', r'\text{Estimation}'),)# faded = True)
 
 #%% Connections
 x.connect('mtow', 'empty', r'W_0')
@@ -45,7 +45,7 @@ x.add_output('mtow', r'W_0^*')
 
 #%% Writing
 x.write(
-        'XDSMMission',     # Filename
+        'XDSMTakeoffEmpty',     # Filename
         build   = True,  # Build PDF using PDFLaTeX
         cleanup = True,  # Clean up TeX junk
         quiet   = False, # Write PDFLaTeX output to terminal
